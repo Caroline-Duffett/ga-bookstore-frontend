d108ff61aaaed750b1f283649671c76426851eaa
CarolineDuffett <ckduffett@gmail.com>
2022-06-14 19:05:29 -0400

    attempt at isolating just what we want to appear inside the card without the map into a component

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -2,23 +2,18 @@ import {useState} from 'react'
 
 const BookCard = (props) => {
 
-
   return (
     <>
-      <h1>Book Card</h1>
-      <div className='all-books-flexbox'>
-        {props.books.map((book) => {
-          return(
-            <div className='book' key={book.id}>
-              <img src={book.cover_art} alt="book cover"/>
-              <h4>Title: {book.title}</h4>
-              <h5>Author: {book.author_name}</h5>
-              <h5>Price: {book.price}</h5>
-           </div>
-         )
-        })}
+      <h1>TESTING</h1>
+      <div className='book' key={book.id}>
+        <img src={book.cover_art} alt="book cover"/>
+        <h4>Title: {book.title}</h4>
+        <h5>Author: {book.author_name}</h5>
+        <h5>Price: {book.price}</h5>
       </div>
-    </>
-)}
+   </>
+ )
+
+}
 
 export default BookCard
