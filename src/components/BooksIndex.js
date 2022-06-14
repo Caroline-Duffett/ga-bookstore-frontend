ad97d4e5c47cddffd07c50ed4fdc436190e4ad6e
CarolineDuffett <ckduffett@gmail.com>
2022-06-14 18:55:08 -0400

    put the original (working) index page into its graveyard. About to really mess around with it

--- src/components/BooksIndex.js
+++ src/components/BooksIndex.js
@@ -46,3 +46,31 @@ export default BooksIndex
 //   </>
 //   : null}
 //------------------------------------------------------------------------------//
+
+
+//------------------------------------------------- Original Books Index page --//
+// import {useState} from 'react'
+//
+// const BooksIndex = (props) => {
+//
+//
+//   return (
+//     <>
+//       <h1>All Books</h1>
+//       <div className='all-books-flexbox'>
+//         {props.books.map((book) => {
+//           return(
+//             <div className='book' key={book.id}>
+//               <img src={book.cover_art} alt="book cover"/>
+//               <h4>Title: {book.title}</h4>
+//               <h5>Author: {book.author_name}</h5>
+//               <h5>Price: {book.price}</h5>
+//            </div>
+//          )
+//         })}
+//       </div>
+//     </>
+// )}
+//
+// export default BooksIndex
+//------------------------------------------------------------------------------//
