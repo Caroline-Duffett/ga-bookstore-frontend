d835727b21e88940ac4f7b53be6cd75dff31969d
CarolineDuffett <ckduffett@gmail.com>
2022-06-14 11:22:57 -0400

    test user state set to admin, working

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,30 +2,34 @@ import {useState} from 'react'
 import Edit from './Edit.js'
 
 const Book = (props) => {
-
+  const [user, setUser] = useState('admin')
   return (
     <>
-    {props.books.map((book) => {
-      return(
-        <div className='book' key={book.id}>
-          <img src={book.cover_art} alt="book cover"/>
-          <h4>Title: {book.title}</h4>
-          <h5>Author: {book.author_name}</h5>
-          <h5>Publisher: {book.publisher}</h5>
-          <h5>Publication date: {book.publication_date}</h5>
-          <h5>Genre: {book.genre}</h5>
-          <h5>Page count: {book.page_count}</h5>
-          <h5>Language: {book.language}</h5>
-          <h5>ISBN: {book.isbn}</h5>
-          <h5>Rating: {book.rating}</h5>
-          <h5>Price: {book.price}</h5>
-          <Edit handleUpdate={props.handleUpdate} book={book}/>
-          <button onClick={() => {props.handleDelete(book)}}>
-          X
-          </button>
-       </div>
-     )
-    })}
+      {props.books.map((book) => {
+        return(
+          <div className='book' key={book.id}>
+            <img src={book.cover_art} alt="book cover"/>
+            <h4>Title: {book.title}</h4>
+            <h5>Author: {book.author_name}</h5>
+            <h5>Publisher: {book.publisher}</h5>
+            <h5>Publication date: {book.publication_date}</h5>
+            <h5>Genre: {book.genre}</h5>
+            <h5>Page count: {book.page_count}</h5>
+            <h5>Language: {book.language}</h5>
+            <h5>ISBN: {book.isbn}</h5>
+            <h5>Rating: {book.rating}</h5>
+            <h5>Price: {book.price}</h5>
+            {user === 'admin' ?
+              <>
+                <Edit handleUpdate={props.handleUpdate} book={book}/>
+                <button onClick={() => {props.handleDelete(book)}}>
+                X
+                </button>
+              </>
+              : null}
+         </div>
+       )
+      })}
     </>
 )}
 
