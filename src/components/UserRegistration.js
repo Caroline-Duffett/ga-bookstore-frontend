02d1deac231f5b6a1943a50e876e4bac9626b3ca
LaffSteven <steve.laff@gmail.com>
2022-06-15 14:01:18 -0400

    started user registration component

---
+++ src/components/UserRegistration.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+const UserRegistration = () => {
+
+    return (
+        <>
+            <h3> New User Registration </h3>
+        </>
+    )
+
+}
+
+export default UserRegistration
