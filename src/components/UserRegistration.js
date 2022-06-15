e805cc7b586a8b7fccbc028fa84e2316ab4cafa6
CarolineDuffett <ckduffett@gmail.com>
2022-06-15 16:50:56 -0400

    finished indents

--- src/components/UserRegistration.js
+++ src/components/UserRegistration.js
@@ -41,78 +41,78 @@ const UserRegistration = (props) => {
         <button onClick={props.signInToggle} className="search-btn">Sign In</button>
         {props.showSignIn ?
           <>
-          <div className="modal-wrapper">
-            <div className="create-acc-si-modal">
-              <div className='create-acc-si-x-btn-div'>
-                <button className='create-acc-si-x-btn' onClick={props.signInToggle}>
-                x
-                </button>
-              </div>
-          {login ?
-            <>
-              <h3>Sign In</h3>
-              <div className="create-acc-si-div">
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="username"> Username: </label>
-                    <input type="text" name="username" onChange={handleChange}/>
-                    <br/>
-                    <br/>
-
-                    <label htmlFor="password"> Password: </label>
-                    <input type="passowrd" name="password" onChange={handleChange}/>
-                    <br/>
-                    <br/>
-                    <div className='add-submit-btn-div'>
-                      <input type='submit'/>
-                    </div>
-                  </form>
+            <div className="modal-wrapper">
+              <div className="create-acc-si-modal">
+                <div className='create-acc-si-x-btn-div'>
+                  <button className='create-acc-si-x-btn' onClick={props.signInToggle}>
+                  x
+                  </button>
                 </div>
-              <div className="account-toggle-btn">
-                <button onClick={loginToggle}>Create Account</button>
-              </div>
-            </>
-            :
-            <>
-                  <h3> New User Registration </h3>
-                  <div className="create-acc-si-div">
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="username"> Username: </label>
-                        <input type="text" name="username" onChange={handleChange}/>
-                        <br/>
-                        <br/>
+                {login ?
+                  <>
+                    <h3>Sign In</h3>
+                    <div className="create-acc-si-div">
+                      <form onSubmit={handleSubmit}>
+                          <label htmlFor="username"> Username: </label>
+                          <input type="text" name="username" onChange={handleChange}/>
+                          <br/>
+                          <br/>
 
-                        <label htmlFor="email"> Email: </label>
-                        <input type="email" name="email" onChange={handleChange}/>
-                        <br/>
-                        <br/>
-
-                        <label htmlFor="password"> Password: </label>
-                        <input type="text" name="password" onChange={handleChange}/>
-                        <br/>
-                        <br/>
-
-                        <label htmlFor="first_name"> First Name: </label>
-                        <input type="text" name="first_name" onChange={handleChange}/>
-                        <br/>
-                        <br/>
-
-                        <label htmlFor="last_name"> Last Name: </label>
-                        <input type="text" name="last_name" onChange={handleChange}/>
-                        <br/>
-                        <br/>
-
-                        <div className='add-submit-btn-div'>
-                          <input type='submit'/>
-                        </div>
-                    </form>
+                          <label htmlFor="password"> Password: </label>
+                          <input type="passowrd" name="password" onChange={handleChange}/>
+                          <br/>
+                          <br/>
+                          <div className='add-submit-btn-div'>
+                            <input type='submit'/>
+                          </div>
+                        </form>
+                      </div>
                     <div className="account-toggle-btn">
-                      <button onClick={loginToggle}>Already have an account? Login</button>
+                      <button onClick={loginToggle}>Create Account</button>
                     </div>
+                  </>
+                  :
+                  <>
+                    <h3> New User Registration </h3>
+                    <div className="create-acc-si-div">
+                      <form onSubmit={handleSubmit}>
+                          <label htmlFor="username"> Username: </label>
+                          <input type="text" name="username" onChange={handleChange}/>
+                          <br/>
+                          <br/>
+
+                          <label htmlFor="email"> Email: </label>
+                          <input type="email" name="email" onChange={handleChange}/>
+                          <br/>
+                          <br/>
+
+                          <label htmlFor="password"> Password: </label>
+                          <input type="text" name="password" onChange={handleChange}/>
+                          <br/>
+                          <br/>
+
+                          <label htmlFor="first_name"> First Name: </label>
+                          <input type="text" name="first_name" onChange={handleChange}/>
+                          <br/>
+                          <br/>
+
+                          <label htmlFor="last_name"> Last Name: </label>
+                          <input type="text" name="last_name" onChange={handleChange}/>
+                          <br/>
+                          <br/>
+
+                          <div className='add-submit-btn-div'>
+                            <input type='submit'/>
+                          </div>
+                      </form>
+                      <div className="account-toggle-btn">
+                        <button onClick={loginToggle}>Already have an account? Login</button>
+                      </div>
+                    </div>
+                  </>
+                }
               </div>
-            </>
-          }
-          </div>
-        </div>
+            </div>
           </>
         : null}
       </>
